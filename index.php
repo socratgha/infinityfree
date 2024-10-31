8a27ee69514f618d129a8fb8be9f4be2841bebda
socratgha <ghsocrat@gmail.com>
2024-10-31 10:16:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Form</title>
+</head>
+<body>
+        <form action="/insert.php" method="get" target="_blank">
+        <label for="first-name">First name:</label>
+        <input type="text" id="first-name" name="first-name"><br><br>
+
+        <label for="last-name">Last name:</label>
+        <input type="text" id="last-name" name="last-name"><br><br>
+
+        <!-- You can add more input fields here if needed -->
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
